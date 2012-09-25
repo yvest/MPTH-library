1db9120e8cfd0eabe0bcbabfec76858f4b2ca88e
Yves Touchette <yves_touchette@hotmail.com>
2012-09-24 20:45:54 -0400

    initial commit

---
+++ MPTH.cpp
@@ -0,0 +1,161 @@
+#include "MPTH.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+#include <Wire.h>  
+
+MPTH::MPTH(uint8_t i2cAddr, uint8_t CmdChar, uint8_t NumLines, uint8_t NumChar) {
+    _i2cAddr = i2cAddr >> 1;
+    _CmdChar = CmdChar;
+    _numlines = NumLines;
+    _numchar = NumChar;
+    _displaymode = 0x0C;
+    _displaycontrol = 0x14;
+    _displayfunction = 0x6;
+    _currline = 0;
+    _currchar = 0;
+}
+
+void MPTH::begin(void) {
+ Wire.begin(); 
+ clear();
+ command(MPTH_LCD_CMD, _displaymode);
+ command(MPTH_LCD_CMD, _displayfunction);
+ command(MPTH_LCD_CMD, _displaycontrol);
+}
+
+inline void MPTH::command(uint8_t cmd, uint8_t value) {
+  byte MPsend[3];
+  MPsend[0] = _CmdChar;
+  MPsend[1] = cmd;
+  MPsend[2] = value;
+  Wire.beginTransmission(_i2cAddr);                  // Send Slave (device) address
+  Wire.write(MPsend, 3);                                  // Send the data
+  Wire.endTransmission();
+}
+
+#if ARDUINO >= 100
+inline size_t MPTH::write(uint8_t value) {
+  if (value == '\n') {
+    _currline++;
+    if (_currline >= _numlines) {
+      home();
+      }
+    else {
+      setCursor(_currline, 0);
+    }   
+  }
+  else {
+    Wire.beginTransmission(_i2cAddr);                  // Send Slave (device) address
+    Wire.write(value);                                  // Send the data
+    Wire.endTransmission();
+    //_currchar++;
+    if (_currchar >= _numchar) {
+      _currchar = 0;
+      _currline++;
+      if (_currline >= _numlines) {
+        _currline = 0;
+        }
+    }
+  }
+  return 1;
+}
+#else
+inline void MPTH::write(uint8_t value) {
+
+}
+#endif
+
+void MPTH::clear()
+{
+  command(MPTH_LCD_CMD, MPTH_LCD_CMDCLEAR);  // clear display, set cursor position to zero
+  _currline = 0;
+  _currchar = 0;
+}
+
+void MPTH::home()
+{
+  command(MPTH_LCD_CMD, MPTH_LCD_CMDHOME);  // set cursor position to zero
+  _currline = 0;
+  _currchar = 0;
+}
+
+void MPTH::setCursor(uint8_t x, uint8_t y){
+ byte pos;
+ pos = (x * _numchar) + y;
+ command(MPTH_MOVECUR,pos);
+ _currline = x;
+ _currchar = y;
+}
+
+void MPTH::noDisplay()
+{
+  _displaymode = _displaymode & 0x0b;
+  command(MPTH_LCD_CMD, _displaymode);  
+}
+void MPTH::display()
+{ 
+  _displaymode = _displaymode | 0x04;
+  command(MPTH_LCD_CMD, _displaymode);  
+}
+
+void MPTH::noBlink()
+{
+  _displaymode = _displaymode & 0x0e;
+  command(MPTH_LCD_CMD, _displaymode);  
+}
+void MPTH::blink()
+{ 
+  _displaymode = _displaymode | 0x01;
+  command(MPTH_LCD_CMD, _displaymode);  
+}
+
+void MPTH::noCursor()
+{
+  _displaymode = _displaymode & 0x0d;
+  command(MPTH_LCD_CMD, _displaymode);  
+}
+void MPTH::cursor()
+{ 
+  _displaymode = _displaymode | 0x02;
+  command(MPTH_LCD_CMD, _displaymode);  
+}
+
+void MPTH::setBacklight(uint8_t status) {
+  command(MPTH_BACKLIGHT, status);
+}
+
+/* void MPTH::scrollDisplayLeft(){
+  command(MPTH_LCD_CMD, status);
+}*/
+
+/*  void MPTH::scrollDisplayRight(){
+  command(MPTH_LCD_CMD, status);
+}*/
+
+void MPTH::leftToRight(){
+  _displaycontrol = _displaycontrol | 0x04;
+  command(MPTH_LCD_CMD, _displaycontrol);
+}
+
+void MPTH::rightToLeft(){
+  _displaycontrol = _displaycontrol & 0x1B;
+  command(MPTH_LCD_CMD, _displaycontrol);
+} 
+
+
+void MPTH::autoscroll() {
+  _displayfunction = _displayfunction | 0x01;
+  command(MPTH_LCD_CMD, _displayfunction);
+}
+void MPTH::noAutoscroll() {
+  _displayfunction = _displayfunction & 0x06;
+  command(MPTH_LCD_CMD, _displayfunction);
+}
+
---
+++ MPTH.h
@@ -0,0 +1,64 @@
+#ifndef MPTH_h
+#define MPTH_h
+
+#include <inttypes.h>
+#include "Print.h"
+//#include "Adafruit_MCP23008.h"
+//#include <Wire.h>
+
+#define MPTH_LCD_CMD 0x05
+#define MPTH_LCD_CMDCLEAR     0x01
+#define MPTH_LCD_CMDHOME      0x02
+#define MPTH_MOVECUR 0x08
+#define MPTH_BACKLIGHT 0x01
+
+
+class MPTH : public Print {
+public:
+
+  MPTH(uint8_t i2cAddr, uint8_t CmdChar, uint8_t NumLines, uint8_t NumChar);
+  void begin(void);
+
+  void clear();
+  void home();
+
+  void noDisplay();
+  void display();
+
+  void noBlink();
+  void blink();
+
+  void noCursor();
+  void cursor();
+  
+// void scrollDisplayLeft();
+//  void scrollDisplayRight();
+ void leftToRight();
+ void rightToLeft();
+ 
+ void autoscroll();
+ void noAutoscroll();
+  
+  // only if using backpack
+  void setBacklight(uint8_t status); 
+
+//  void createChar(uint8_t, uint8_t[]);
+  void setCursor(uint8_t, uint8_t); 
+#if ARDUINO >= 100
+  virtual size_t write(uint8_t);
+#else
+  virtual void write(uint8_t);
+#endif
+  void command(uint8_t, uint8_t);
+private:
+
+  uint8_t _displayfunction; // auto-scroll
+  uint8_t _numlines, _currline, _numchar, _currchar;
+  uint8_t _displaymode; // Display enable, cursor enable, cursor blink
+  uint8_t _displaycontrol; //Display shift, Direction of shift
+
+  uint8_t _i2cAddr;
+  uint8_t _CmdChar;
+};
+
+#endif
